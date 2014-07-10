9e727674f88a6d93515b470d57966b7ebe03ad78
gvraaphorst <gvraaphorst@sogyo.nl>
2014-07-10 11:05:34 +0200

    Initial commit

---
+++ src/gvr/itemfusion/Fusion.java
@@ -0,0 +1,7 @@
+package gvr.itemfusion;
+
+/**
+ * Created by gvraaphorst on 10/07/2014.
+ */
+public class Fusion {
+}
---
+++ tests/gvr/itemfusion/tests/ItemTest.java
@@ -0,0 +1,7 @@
+package gvr.itemfusion.tests;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+}
